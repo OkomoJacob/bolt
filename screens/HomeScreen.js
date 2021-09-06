e69ebb710fba20d9f375cd05e144a07f96a67d16
okomojacob <jaylabs2030@gmail.com>
2021-09-06 18:48:44 +0300

    fixed :bug:, added :logo:

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,24 @@
 /* React functional component */
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 
 const HomeScreen = () =>{
     return (
-        <SafeAreaView style={tw`bg-red-900 h-full`}>
-            <Text style={tw`text-red-500 p-10`}>I am HomeScreen</Text>
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain"
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs",
+                    }}
+                />
+            </View>
         </SafeAreaView>
     );
 };
