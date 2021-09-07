859b724a014946b77966db8216c75ef5c52f892d
okomojacob <jaylabs2030@gmail.com>
2021-09-07 12:30:49 +0300

    fixed :bug:, added :logo:, and TouchOpacity

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+
 
 const data = [
     {
@@ -25,12 +27,15 @@ const NavOptions = () => {
             keyExtractor = {(item) => item.id}
             renderItem={({ item }) => (
                 // Opacity changes on touch
-                <TouchableOpacity>
+                <TouchableOpacity
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                >
                     <View>
                         <Image
                             style={{ width: 120, height: 120, resizeMode: "contain"}}
                             source={{ uri: item.image }}
                         />
+                        <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
                     </View>
                 </TouchableOpacity>
             )}
